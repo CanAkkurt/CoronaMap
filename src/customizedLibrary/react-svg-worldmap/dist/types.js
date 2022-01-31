568ded667046fc22656f9b9292957e175128729e
KADIR AKKURT <kadir.akkurt@student.hogent.be>
2022-02-01 00:37:07 +0100

    dele

--- src/customizedLibrary/react-svg-worldmap/dist/types.js
+++
@@ -1,5 +0,0 @@
-
-Object.defineProperty(exports, "__esModule", { value: true });
-const countries_geo_1 = require("./countries.geo");
-// eslint-disable-next-line no-unused-vars
-const isoCodes = countries_geo_1.default.features.map(({ I }) => I);
